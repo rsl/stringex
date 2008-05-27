7865041b5a5ef6f33f7ff7770549543bed7c4023
rsl <rsl@luckysneaks.com>
2008-05-27 09:07:55 -0400

    added note about expense of method

--- lib/lucky_sneaks/acts_as_url.rb
+++ lib/lucky_sneaks/acts_as_url.rb
@@ -38,6 +38,10 @@ module LuckySneaks
       # Initialize the url fields for the records that need it. Designed for people who add
       # <tt>acts_as_url</tt> support once there's already development/production data they'd
       # like to keep around.
+      # 
+      # Note: This method can get very expensive, very fast. If you're planning on using this
+      # on a large selection, you will get much better results writing your own version with
+      # using pagination.
       def initialize_urls
         find(:all, :conditions => {:url => nil}).each do |instance|
           instance.send :ensure_unique_url
