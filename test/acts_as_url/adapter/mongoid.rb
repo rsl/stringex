3d8004ed561bafaf0b07ca315fc6a9197f783f2c
RSL <sconds@gmail.com>
2012-12-06 17:23:09 -0500

    oops forgot to undo this fubar optimization on the mongo side

--- test/acts_as_url/adapter/mongoid.rb
+++ test/acts_as_url/adapter/mongoid.rb
@@ -17,3 +17,77 @@ class Document
 
   acts_as_url :title
 end
+
+class Updatument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+
+  acts_as_url :title, :sync_url => true
+end
+
+class Mocument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+
+  acts_as_url :title, :scope => :other, :sync_url => true
+end
+
+class Permument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+
+  acts_as_url :title, :url_attribute => :permalink
+end
+
+class Procument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+
+  acts_as_url :non_attribute_method
+
+  def non_attribute_method
+    "#{title} got massaged"
+  end
+end
+
+class Blankument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+
+  acts_as_url :title, :only_when_blank => true
+end
+
+class Duplicatument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+
+  acts_as_url :title, :duplicate_count_separator => "---"
+end
+
+class Validatument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+
+  acts_as_url :title, :sync_url => true
+  validates_presence_of :title
+end
+
+class Ununiqument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+
+  acts_as_url :title, :allow_duplicates => true
+end
+
+class Limitument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+
+  acts_as_url :title, :limit => 13
+end
+
+class Skipument
+  include Mongoid::Document
+  Stringex::ActsAsUrl.mix_into self
+  acts_as_url :title, :exclude => ["_So_Fucking_Special"]
+end
