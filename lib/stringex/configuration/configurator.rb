53aa9b641bdd881e0a6b51de09c19f0ff610c390
RSL <sconds@gmail.com>
2013-01-03 10:40:54 -0500

    try to fix 1.8.7 issue with interning symbols

--- lib/stringex/configuration/configurator.rb
+++ lib/stringex/configuration/configurator.rb
@@ -12,10 +12,11 @@ module Stringex
       end
 
       def define_instance_method_for_configuration_wrapper(name)
+        name = name.respond_to?(:intern) ? name.intern : name
         (class << self; self; end).instance_eval do
           define_method("#{name}=") do |value|
             customizations = klass.send(:system_wide_customizations)
-            customizations[name.intern] = value
+            customizations[name] = value
           end
         end
       end
